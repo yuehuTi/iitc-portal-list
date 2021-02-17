e6afa94443ff0bbacf07c87aa3be5d8b26ac98a6
yuehuTi <yuehu_p@hotmail.com>
2021-02-17 23:50:48 +0800

    modify: update url

---
+++ portals-list.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @id             iitc-plugin-portals-list@teo96 edit by takurua/yuehuTi
+// @name           IITC plugin: show list of portals
+// @category       Info
+// @version        0.2.1.20210217.234100
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://github.com/yuehuTi/iitc-portal-list/raw/main/portals-list.meta.js
+// @downloadURL    https://github.com/yuehuTi/iitc-portal-list/raw/main/portals-list.user.js
+// @description    [iitc-2017-01-08-021732] Display a sortable list of all visible portals with full details about the team, resonators, links, etc.
+// @include        *://*.ingress.com/intel*
+// @include        *://intel.ingress.com/*
+// @match          *://*.ingress.com/intel*
+// @match          *://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
--- portals-list.user.js
+++ portals-list.user.js
@@ -4,17 +4,13 @@
 // @category       Info
 // @version        0.2.1.20210217.234100
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @updateURL      https://static.iitc.me/build/release/plugins/portals-list.meta.js
-// @downloadURL    https://static.iitc.me/build/release/plugins/portals-list.user.js
+// @updateURL      https://github.com/yuehuTi/iitc-portal-list/raw/main/portals-list.meta.js
+// @downloadURL    https://github.com/yuehuTi/iitc-portal-list/raw/main/portals-list.user.js
 // @description    [iitc-2017-01-08-021732] Display a sortable list of all visible portals with full details about the team, resonators, links, etc.
-// @include        https://*.ingress.com/intel*
-// @include        http://*.ingress.com/intel*
-// @match          https://*.ingress.com/intel*
-// @match          http://*.ingress.com/intel*
-// @include        https://*.ingress.com/mission/*
-// @include        http://*.ingress.com/mission/*
-// @match          https://*.ingress.com/mission/*
-// @match          http://*.ingress.com/mission/*
+// @include        *://*.ingress.com/intel*
+// @include        *://intel.ingress.com/*
+// @match          *://*.ingress.com/intel*
+// @match          *://intel.ingress.com/*
 // @grant          none
 // ==/UserScript==
 
